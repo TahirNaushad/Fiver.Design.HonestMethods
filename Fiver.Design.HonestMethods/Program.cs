92f27b460e0bfef2abba905d4c6d9fb8b58a39ee
Tahir Naushasd <t.naushad25@gmail.com>
2018-05-06 09:20:58 +0100

    Add project files.

---
+++ Fiver.Design.HonestMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiver.Design.HonestMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
